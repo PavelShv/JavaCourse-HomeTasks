5aedf8ba8618454a2f81479f45ca42cf96edc038
pavel.shvedov <compser.shvedov@gmail.com>
2022-07-20 23:17:25 +0200

    Задание №1 проработал и прослушал по лекции. Вставлял разные значения - все запускается

---
+++ Lesson2/src/main/java/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+        public final static String CHIS = "CHIS";
+        public final static String DELIMETER = ":";
+        public final static String TEST = ":";
+
+    }
+
---
+++ Lesson2/src/main/java/Practice1.java
@@ -0,0 +1,44 @@
+public class Practice1 {
+    public static final String TEMP = "c:\\temp";
+
+
+    public static void main(String[] args) {
+        System.out.println("Privet! \"Minsk\" \nKak dela?");
+        long s = 1000000000;
+
+        int myCount = 5 + 0b1111;
+        char c = 'v';
+        System.out.println(c);
+
+        int c1 = 1;
+        final int k = 1;
+        //int k = 2;
+
+        test();
+        int b = 1;
+        c = '1';
+        System.out.println(c);
+
+        float f = 2.2f;
+        double d = 4.4;
+
+        long c2 = 123112343563653434L;
+        char ch = '\u0066';
+        System.out.println(ch);
+ //   Byte, Short, Integer, Long, Float, Double, Character, Boolean
+//
+//      float
+//      double
+        boolean b2 = false;//0
+        boolean b1 = true;//1
+
+        String str = "";
+
+}
+
+    private static void test() {
+        boolean b = false;//0
+        boolean b1 = true;//1
+        System.out.println("sdf");
+    }
+    }
